99a481e0e2e5d7946f93dad7772808c1dbff5844
Jorgearh <jorgerompiche@gmail.com>
2021-12-02 22:58:37 -0600

    Inmutabilidad arreglos

--- inmutable.js
+++ inmutable.js
@@ -1,3 +1,6 @@
+//inmutabilidad: no modificar el objeto original
+
+
 const juan = {
     nombre: "Juan",
     apellido: "Rodriguez",
@@ -24,5 +27,33 @@ const juan2 = {
 
 //juan2.direccion.municipio = "Santa Catarina Pinula"
 
-console.log("juan:", juan);
-console.log("juan2: ",juan2);
+//console.log("juan:", juan);
+//console.log("juan2: ",juan2);
+
+
+//Arreglos inmutables
+
+const numeros = [1, 2, 3];
+
+//const numeros2 = numeros;// copia la referencia al arreglo original
+//const numeros2 = [...numeros, 4];
+//numeros2.push(5);
+//const numeros2 = [0,...numeros, 4];//agregar elemento al inicio
+const numeros2 = [0, ...numeros, 4, ...numeros];//agregar dos arreglos
+
+const index = numeros.indexOf(2);   //agregar elementos intermedios
+const numeros3 = [                  
+    ...numeros.slice(0, index),
+    1.5,
+    ...numeros.slice(index,)
+]
+
+const numeros4 = numeros.filter(x => x != 2);   //eliminar un valor
+
+const numeros5 = numeros.map(x => x == 2 ? 100 : x);
+
+console.log("numeros:", numeros);
+console.log("numeros2:", numeros2);
+console.log("numeros3:", numeros3);
+console.log("numeros4:", numeros4);
+console.log("numeros5:", numeros5);
