74760a7ce845e19a55e99febb28161cbfa3f31f3
Jorgearh <jorgerompiche@gmail.com>
2021-12-09 23:14:53 -0600

    Redux para hacer render de la tabla productos

--- index-v2.js
+++ index-v2.js
@@ -57,6 +57,17 @@ const reducer = (state, action) => {
         };
     }
 
+
+    if(action.type == "producto-eliminado")
+    {
+        const codigo = action.payload.codigo;
+        const productos = state.productos.filter((item) => item.codigo != codigo);
+        return {
+            ...state,
+            productos
+        }
+    }
+
     return state;
 };
 
@@ -86,15 +97,27 @@ function renderTable(productos)
             <td>${item.total}</td>
             <td>
                 <div class="btn-group">
-                    <a title="Editar" href="#" onclick="onEdit(event)" class="btn btn-sm btn-outline-secondary">
+                    <a title="Editar" href="#" class="btn btn-sm btn-outline-secondary">
                         <i class="bi bi-pencil-square"></i>
                     </a> 
-                    <a title="Eliminar" href="#" onclick="onDelete(event)" class="btn btn-sm btn-outline-danger">
+                    <a title="Eliminar" href="#" class="btn btn-sm btn-outline-danger">
                         <i class="bi bi-trash"></i>
                     </a>
                 </div>
             </td>
         `;
+
+        const [editar, eliminar] = tr.getElementsByTagName("a");
+
+        eliminar.addEventListener("click", (event) => {
+            event.preventDefault();
+            store.dispatch({
+                type: "producto-eliminado",
+                payload: {
+                    codigo: item.codigo
+                }
+            })
+        });
         return tr;
     });
 
@@ -102,6 +125,17 @@ function renderTable(productos)
     filas.forEach((tr) => {
         tbody.appendChild(tr); 
     });
+
+    cantidadTotalElement.innerText = sum(productos, x => x.cantidad);
+    precioTotalElement.innerText = sum(productos, x => x.precio);
+    granTotalElement.innerText = sum(productos, x => x.total);
+
+    function sum(elementos, selector)
+    {
+        return elementos
+            .map(selector)
+            .reduce((a, b) => a + b, 0);// 0 + a, a + b, ...
+    }
 }
 
 store.dispatch({
@@ -142,5 +176,12 @@ store.dispatch({
         cantidad: 2,
         precio: 4,
         categoria: 4
+    }
+});
+
+store.dispatch({
+    type: "producto-eliminado",
+    payload: {
+        codigo: 2
     }
 });
